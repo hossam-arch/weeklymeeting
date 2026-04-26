c2343aeb691f4a39b2d49068600a33af0a20cee9
hossam-arch <hossam@orcas.io>
2026-04-26 21:07:59 +0300

    Update config.js

--- components/config.js
+++ components/config.js
@@ -9,5 +9,5 @@
 window.BGH_CONFIG = {
   supabaseUrl:  'https://usqwgrwosfckzjxynwpd.supabase.co',
   supabaseKey:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzcXdncndvc2Zja3pqeHlud3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMDAzMTIsImV4cCI6MjA5Mjc3NjMxMn0.wJERRdKQwHPedFBRAcpMTHLeIWuyGHB-4ZJ9vHeoGAA',
-  anthropicKey: 'YOUR_ANTHROPIC_API_KEY',
+  anthropicKey: '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',
 };
